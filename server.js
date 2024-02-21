7f00c3030f7db3c81ca51de07140c8b1fcc2fb3e
mariamgamal70 <mariamgamal70@gmail.com>
2024-02-21 22:29:20 +0200

    created server.js
    required modules required for backend
    connected server and mongodb atlas

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("db connection success");
+  })
+  .catch((err) => {
+    console.log("ERROR");
+  });
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => {
+  console.log(`app running on port ${port}...`);
+});
