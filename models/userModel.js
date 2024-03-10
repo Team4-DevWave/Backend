1aefe7e8153af38d5b09081ad4b596d5eb7cf374
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-10 21:50:40 +0200

    added middleware password encyption

--- models/userModel.js
+++ models/userModel.js
@@ -152,6 +152,16 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// middlewares
+//password encryption
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
 
 
 const userModel = mongoose.model("users", userSchema);
