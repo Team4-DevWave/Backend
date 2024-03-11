f2fc82e362cab2dc5edd040e699a99a56d880d38
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-11 16:41:32 +0200

    added chatroom model

---
+++ models/chatroommodel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const chatroomSchema = new mongoose.Schema({
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+  },
+  chatroomName: {
+    type: String,
+  },
+    chatroomMembers: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "users",
+        },
+    ],
+    chatroomMessages: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "chatmessages",
+        },
+    ],
+    chatroomAdmin: 
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "users",
+        },
+
+});
+
+const chatroomModel = mongoose.model("chatrooms", chatroomSchema);
+
+module.exports = chatroomModel;
