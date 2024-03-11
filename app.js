fa2a4b2acc96100ca25172288a8f1b6dafc9c5b1
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-11 23:46:11 +0200

    added several routes in app.js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/r", subredditRouter);
+app.use("/api/v1/messages", messegeRouter);
+
 
 app.all("*", (req, res, next) => {
   next(new appError(`cant find ${req.originalUrl} on this server!`, 400)); //<< if u pass error its going to know that its going to stop the whole program and go to the error middleware
