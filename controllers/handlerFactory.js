e0e055bd72a084c656d6598b49bac97fa5dab46c
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-12 00:03:40 +0200

    added handlerfactory.js
    handler factory functions is a function that returns another function it decreases code repetition in routing

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,81 @@
+const catchAsync = require("./../utils/catchAsync");
+const APIFeatures = require("./../utils/apiFeatures");
+const appError = require("../utils/appError");
+
+exports.deleteOne = (model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await model.findByIdAndDelete(req.params.id);
+    if (!doc) {
+      return next(new appError("no document with that id", 404));
+    }
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  });
+
+exports.updateOne = (model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!doc) {
+      return next(new appError("no document with that id", 404));
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.createOne = (model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await model.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (model, populateOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = model.findById(req.params.id);
+    if (populateOptions) {
+      query = query.populate(populateOptions);
+    }
+    const doc = await query;
+    if (!doc) {
+      return next(new appError("no document with that id", 404));
+    }
+    res.status(200).json({
+      status: "success",
+      result: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (model) =>
+  catchAsync(async (req, res, next) => {
+    // let filter = {};
+    // if (req.params.tourId) filter = { tour: req.params.tourId }; //if  there is a param in the url then we apply that filter to get reviews for that specific tour only , else if filter is empty just get all reviews
+    //execute query
+    const features = new APIFeatures(model.find(), req.query); //.find returns query, .aggregate returns object
+    features.filter().sort().limitFields().paginate();
+    const doc = await features.query;
+    //query.sort().select().skip().limit()
+    //send response
+    res.status(200).json({
+      status: "success",
+      result: doc.length,
+      data: {
+        doc,
+      },
+    });
+  });
