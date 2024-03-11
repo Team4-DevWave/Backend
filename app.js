57f7a53cd1bea472bdcd0dc1b9c44c99174f1cb0
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-11 23:56:25 +0200

    Update app.js
    added global error handling  (Error controller)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const userRouter = require("./routes/userroutes.js");
 const appError = require("./utils/appError.js");
+const globalErrorHandler = require("./controllers/errorController.js");
 const app = express();
 
 app.use("/api/v1/users", userRouter);
@@ -15,4 +16,5 @@ app.all("*", (req, res, next) => {
   next(new appError(`cant find ${req.originalUrl} on this server!`, 400)); //<< if u pass error its going to know that its going to stop the whole program and go to the error middleware
 });
 
+app.use(globalErrorHandler);
 module.exports = app;
