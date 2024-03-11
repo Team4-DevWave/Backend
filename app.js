5374c32396a091bbcfee2b1328b8efe73b10adc5
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-11 23:42:02 +0200

    updated app.js
    added user router
    added global route handler

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new appError(`cant find ${req.originalUrl} on this server!`, 400)); //<< if u pass error its going to know that its going to stop the whole program and go to the error middleware
+});
 
 module.exports = app;
