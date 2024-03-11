fb7fdec30d81fb3a41e08d3dc1347019360d1a86
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-11 23:42:29 +0200

    setting up userroutes

---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const userController = require("./../controllers/userController");
+const authController = require("./../controllers/authController");
+const userRouter = express.Router();
+
+
+
+module.exports = userRouter;
