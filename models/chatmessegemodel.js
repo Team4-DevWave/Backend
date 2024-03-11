be34b17411e1261cd35a44024a096459be5971d6
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-11 16:45:25 +0200

    created chat messages model

---
+++ models/chatmessegemodel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const chatMessageSchema = new mongoose.Schema({
+  dateSent: {
+    type: Date,
+    default: Date.now(),
+  },
+  sender: {
+    type: mongoose.Schema.ObjectId,
+    ref: "users",
+    required: [true, "please enter a sender username"],
+  },
+    reciever: [{
+        type: mongoose.Schema.ObjectId,
+        ref: "users",
+        required: [true, "please enter a reciever username"],
+    }],
+    message: {
+        type: String,
+        required: [true, "please enter a message"],
+    },
+});
+
+const chatMessageModel = mongoose.model(
+  "chatmessages",
+  chatMessageSchema
+);
+
+module.exports = chatMessageModel;
