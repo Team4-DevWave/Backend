6cf05557044224d0b8ebdc7f2d3351ba3a0c9b6a
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-12 13:46:33 +0200

    added check username availability route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,9 @@ const userController = require("./../controllers/usercontroller");
 const authController = require("./../controllers/authcontroller");
 const userRouter = express.Router();
 
-
+userRouter.get(
+  "/username_available/:username",
+  userController.usernameAvailable
+);
 
 module.exports = userRouter;
