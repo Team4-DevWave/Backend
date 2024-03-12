63c7298915ccdf1ad5619f3d00e0d296611978d2
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-13 00:29:28 +0200

    app.js update
    added other routes (post,comment, subreddit, message)
    adjusted naming of messege route

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const express = require("express");
 const userRouter = require("./routes/userroutes.js");
+const postRouter = require("./routes/postroutes.js");
+const commentRouter = require("./routes/commentroutes.js");
+const subredditRouter = require("./routes/subredditroutes.js");
+const messegeRouter = require("./routes/messegeroutes.js");
 const appError = require("./utils/apperror.js");
 const globalErrorHandler = require("./controllers/errorcontroller.js");
 const app = express();
@@ -9,7 +13,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/r", subredditRouter);
-app.use("/api/v1/messages", messegeRouter);
+app.use("/api/v1/messeges", messegeRouter);
 
 app.all("*", (req, res, next) => {
   next(new appError(`cant find ${req.originalUrl} on this server!`, 400)); //<< if u pass error its going to know that its going to stop the whole program and go to the error middleware
