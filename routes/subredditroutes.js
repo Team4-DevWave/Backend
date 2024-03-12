2481ba5aa7b38ac7a992a368b44e93d8533f5de1
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-13 00:46:45 +0200

    added subreddit controller and routes

---
+++ routes/subredditroutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const subredditController = require("../controllers/subredditcontroller");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(subredditController.getAllSubreddits)
+  .post(subredditController.createSubreddit);
+
+
+module.exports = router;
