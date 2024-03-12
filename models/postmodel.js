adae9da90b1a58cb803b2eec4a43d160cff36d05
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-12 14:20:23 +0200

    added 2 new fields to postmodel
    locked and followed booleans

--- models/postmodel.js
+++ models/postmodel.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
 const postSchema = mongoose.Schema({
-  commentsID: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-  userID: { type: mongoose.Schema.Types.ObjectId, ref:'User',required: true},
+  commentsID: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   postedTime: { type: Date, required: true },
   numViews: { type: Number, required: true },
-  subredditID: { type: mongoose.Schema.Types.ObjectId, ref: 'Subreddit' },
+  subredditID: { type: mongoose.Schema.Types.ObjectId, ref: "Subreddit" },
   title: { type: String, required: true },
-  type: { type: String, enum:["poll","image/video","text"],required: true },
+  type: { type: String, enum: ["poll", "image/video", "text"], required: true },
   spoiler: { type: Boolean, required: true },
   nsfw: { type: Boolean, required: true },
   lastEditedTime: { type: Date, required: true },
   votes: { type: Number, required: true },
   content: { type: String, required: true },
-})
+  locked: { type: Boolean, required: false },
+  followed: { type: Boolean, required: false },
+});
 
 const postModel = mongoose.model("Review", postSchema);
 export default postModel;
