7e794a4c191e743e5e106079cbd1b6a532a4b223
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-13 23:10:08 +0200

    removed parameters in connect DB

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(DB)
   .then(() => {
     console.log("db connection success");
   })
@@ -22,3 +22,4 @@ const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
+
