3f07db4e16303a6c142fe1ded0a73f2647f4fb46
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-15 22:35:04 +0200

    save comment is patch instead of post

--- routes/commentroutes.js
+++ routes/commentroutes.js
@@ -16,7 +16,7 @@ commentRouter
     .patch(commentController.editComment)
     .delete(commentController.deleteComment);
 commentRouter.route('/:id/report').post(commentController.reportComment);
-commentRouter.route('/:id/save').post(commentController.saveComment);
+commentRouter.route('/:id/save').patch(commentController.saveComment);
 commentRouter.route('/:id/vote').patch(commentController.voteComment);
 
 module.exports = commentRouter;
