30c7aab4d31c7524738d40e8a83dc949b2ab1035
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-16 04:45:21 +0200

    getmessege,sent,unread,inbox,compose done

--- routes/messageroutes.js
+++ routes/messageroutes.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const messageController = require('../controllers/messagecontroller');
-
+const authController = require('../controllers/authcontroller');
 const router = express.Router();
 
+router.use(authController.protect);
 router
-    .route('/')
-    .get(messageController.getAllmessages)
-    .post(messageController.createmessage);
+    .route('/:id')
+    .get(messageController.getMessage);
+router
+    .route('/inbox')
+    .get(messageController.getInbox);
+router
+    .route('/sent')
+    .get(messageController.getSent);
+router
+    .route('/unread')
+    .get(messageController.getUnread);
+router
+    .route('/compose')
+    .post(messageController.createMessage);
 
 module.exports = router;
