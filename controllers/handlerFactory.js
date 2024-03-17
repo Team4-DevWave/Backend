e27d8caf232fbf8bb3116233c1761280d17904b2
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-18 01:29:12 +0200

    added adjustments to generic func to accept async

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -16,7 +16,11 @@ exports.deleteOne = (model) =>
 
 exports.updateOne = (model, modifyReqBody = (req) => req.body) =>
   catchAsync(async (req, res, next) => {
-    const doc = await model.findByIdAndUpdate(req.params.id, {$set: modifyReqBody(req)}, {
+    let modifiedBody = modifyReqBody(req);
+    if (modifiedBody instanceof Promise) {
+      modifiedBody = await modifiedBody;
+    }
+    const doc = await model.findByIdAndUpdate(req.params.id, {$set: modifiedBody}, {
       new: true,
       runValidators: true,
     });
@@ -31,7 +35,11 @@ exports.updateOne = (model, modifyReqBody = (req) => req.body) =>
 
 exports.createOne = (model, modifyReqBody = (req) => req.body) =>
   catchAsync(async (req, res, next) => {
-    const doc = await model.create(modifyReqBody(req));
+    let modifiedBody = modifyReqBody(req);
+    if (modifiedBody instanceof Promise) {
+      modifiedBody = await modifiedBody;
+    }
+    const doc = await model.create(modifiedBody);
     res.status(201).json({
       status: 'success',
       data: doc,
@@ -67,4 +75,63 @@ exports.getAll = (model, filterFunc = () => ({})) =>
     });
   });
 
+exports.voteOne=(model, voteOn)=> catchAsync(async (req, res, next) => {
+  const voteType= req.body.voteType;
+  if (voteType!==1 && voteType!==-1) {
+    return next(new AppError('invalid vote type', 400));
+  }
+  const doc= await model.findById(req.params.id);
+  if (!doc) {
+    return next(new AppError('no document with that id', 404));
+  }
+  let uservote;
+  if (req.user.upvotes[voteOn].includes(req.params.id)) {
+    uservote=1;
+  } else if (req.user.downvotes[voteOn].includes(req.params.id)) {
+    uservote=-1;
+  } else {
+    uservote=0;
+  }
+  if (voteType==uservote) {
+    doc.votes-=voteType;
+    if (voteType==1) {
+      req.user.upvotes[voteOn].pull(req.params.id);
+    } else if (voteType==-1) {
+      req.user.downvotes[voteOn].pull(req.params.id);
+    }
+  } else if (voteType==-uservote) {
+    doc.votes+=2*voteType;
+    if (voteType==1) {
+      req.user.upvotes[voteOn].push(req.params.id);
+      req.user.downvotes[voteOn].pull(req.params.id);
+    } else if (voteType==-1) {
+      req.user.downvotes[voteOn].push(req.params.id);
+      req.user.upvotes[voteOn].pull(req.params.id);
+    }
+  } else {
+    doc.votes+=voteType;
+    if (voteType==1) {
+      req.user.upvotes[voteOn].push(req.params.id);
+    } else if (voteType==-1) {
+      req.user.downvotes[voteOn].push(req.params.id);
+    }
+  }
+  await doc.save();
+  await req.user.save();
+  res.status(200).json({
+    status: 'success',
+  });
+});
 
+exports.checkMentions=async (model, content)=> {
+  const mentionRegex = /(?:r\/|u\/)(\w+)/g;
+  let match;
+  const mentionedUsers = [];
+  while ((match = mentionRegex.exec(content)) !== null) {
+    const mentionedUser = await model.findOne({username: match[1]});
+    if (mentionedUser) {
+      mentionedUsers.push(mentionedUser.id);
+    }
+  }
+  return mentionedUsers;
+};
