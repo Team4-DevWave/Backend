5c699db7b27eeefd864d746909072ca686272aaa
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-18 02:35:02 +0200

    added hide and save post

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -2,6 +2,7 @@ const postModel = require('../models/postmodel');
 const userModel = require('../models/usermodel');
 const catchAsync = require('../utils/catchasync');
 const handlerFactory = require('./handlerfactory');
+const AppError = require('../utils/apperror');
 
 exports.getPosts = handlerFactory.getAll(postModel);
 exports.getPost = handlerFactory.getOne(postModel);
@@ -17,7 +18,26 @@ exports.editPost = handlerFactory.updateOne(postModel, async (req) => {
 });
 exports.deletePost = handlerFactory.deleteOne(postModel);
 exports.vote = exports.voteComment = handlerFactory.voteOne(postModel, 'posts');
-exports.savePost = catchAsync(async (req, res, next) => {});
-exports.hidePost = catchAsync(async (req, res, next) => {});
+exports.savePost = catchAsync(async (req, res, next) => {
+  const post= await postModel.findById(req.params.id);
+  if (!post) {
+    return next(new AppError('no post with that id', 404));
+  }
+  post.saved = !post.saved;
+  await post.save();
+  const update= post.saved ?
+    {$addToSet: {'savedPostsAndComments.posts': req.params.id}} :
+    {$pull: {'savedPostsAndComments.posts': req.params.id}};
+  await userModel.findByIdAndUpdate(req.user.id, update, {new: true});
+  res.status(200).json({
+    status: 'success',
+    data: {post,
+    },
+  });
+});
+exports.hidePost = handlerFactory.updateOne(postModel, async (req) => {// hide/unhide post
+  req.body.hidden = !req.body.hidden;
+  return req.body;
+});
 exports.reportPost = catchAsync(async (req, res, next) => {});
 exports.crosspost = catchAsync(async (req, res, next) => {});
