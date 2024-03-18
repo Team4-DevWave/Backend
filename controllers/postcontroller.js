37705f7875e446dfacfcf04869cfc4d9fad10fc6
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-18 02:23:23 +0200

    Added vote, adjusted create andedit post to have mentions

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,13 +1,22 @@
 const postModel = require('../models/postmodel');
+const userModel = require('../models/usermodel');
 const catchAsync = require('../utils/catchasync');
 const handlerFactory = require('./handlerfactory');
 
 exports.getPosts = handlerFactory.getAll(postModel);
 exports.getPost = handlerFactory.getOne(postModel);
-exports.createPost = handlerFactory.createOne(postModel);
-exports.editPost = handlerFactory.updateOne(postModel);
+exports.createPost = handlerFactory.createOne(postModel, async (req) => {
+  req.body.user = req.user.id;
+  req.body.mentioned=await handlerFactory.checkMentions(userModel, req.body.content);
+  return req.body;
+});
+exports.editPost = handlerFactory.updateOne(postModel, async (req) => {
+  req.body.lastEditedTime = Date.now();
+  req.body.mentioned= await handlerFactory.checkMentions(userModel, req.body.content);
+  return req.body;
+});
 exports.deletePost = handlerFactory.deleteOne(postModel);
-exports.vote = catchAsync(async (req, res, next) => {});
+exports.vote = exports.voteComment = handlerFactory.voteOne(postModel, 'posts');
 exports.savePost = catchAsync(async (req, res, next) => {});
 exports.hidePost = catchAsync(async (req, res, next) => {});
 exports.reportPost = catchAsync(async (req, res, next) => {});
