804f85fdcd628b83a802877644ac29515c2159c8
mariamgamal70 <mariamgamal70@gmail.com>
2024-03-26 03:57:20 +0200

    removed get posts from post routes
    u can get all posts of a certain sth for example user or subreddit

--- routes/postroutes.js
+++ routes/postroutes.js
@@ -2,17 +2,15 @@ const express = require('express');
 const postController = require('./../controllers/postcontroller');
 // const authController = require("./../controllers/authcontroller");
 const commentRouter = require('./commentroutes');
-// const authController = require('./../controllers/authcontroller');
 // eslint-disable-next-line new-cap
 const postRouter = express.Router();
 postRouter.use('/:id/comments', commentRouter);
-
 // postRouter.use(authController.checkSubredditAccess('post'));
 
 postRouter
     .route('/')
-    .post(postController.createPost)
-    .get(postController.getPosts);
+    .post(postController.createPost);
+// .get(postController.getPosts);
 postRouter.get('/:id', postController.getPost);
 postRouter.delete('/:id/delete', postController.deletePost);
 postRouter.delete('/:id/unhide', postController.unhidePost);
