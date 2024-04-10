ce320ffe491e68899834e779b8aa2292f9d8046f
mariamgamal70 <mariamgamal70@gmail.com>
2024-04-10 22:38:12 +0200

    added cors

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ const AppError = require('./utils/apperror.js');
 const globalErrorHandler = require('./controllers/errorcontroller.js');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/comments', commentRouter);
 app.use('/api/v1/r', subredditRouter);
 app.use('/api/v1/messages', messageRouter);
-
 
 app.all('*', (req, res, next) => {
   next(new AppError(`cant find ${req.originalUrl} on this server!`, 400));
