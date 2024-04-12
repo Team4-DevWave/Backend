88c83a6f603401686bfafb639d968c19995dacfb
mariamgamal70 <mariamgamal70@gmail.com>
2024-04-12 22:55:06 +0200

    comment user population done

--- controllers/commentcontroller.js
+++ controllers/commentcontroller.js
@@ -43,7 +43,7 @@ const createMessage = catchAsync(async (comment) => {
   if (comment.mentioned && comment.mentioned.length > 0) {
     comment.mentioned.forEach(async (userId) => {
       // If the user is the one who created the comment, skip sending the message
-      if (userId.toString() === comment.user.toString()) {
+      if (userId.toString() === comment.user.id) {
         return;
       }
       const user = await userModel.findById(userId);
@@ -51,7 +51,7 @@ const createMessage = catchAsync(async (comment) => {
         throw new AppError('User not found', 404);
       }
       await messageModel.create({
-        from: comment.user,
+        from: comment.user._id,
         fromType: 'users',
         to: userId,
         toType: 'users',
@@ -68,9 +68,9 @@ const createMessage = catchAsync(async (comment) => {
     throw new AppError('Post not found', 404);
   }
   // Check if the comment's user is not the post's owner
-  if (comment.user.toString() !== post.userID.toString()) {
+  if (comment.user.id !== post.userID) {
     await messageModel.create({
-      from: comment.user,
+      from: comment.user._id,
       fromType: 'users',
       to: post.userID,
       toType: 'users',
@@ -120,7 +120,7 @@ exports.editComment = catchAsync(async (req, res, next) => {
   if (!comment) {
     return next(new AppError('no comment with that id', 404));
   }
-  if (comment.user.toString() != req.user.id.toString()) {
+  if (comment.user.id != req.user.id) {
     return next(new AppError('you are not allowed to edit this comment', 403));
   }
   const oldMentions = comment.mentioned;
@@ -148,7 +148,7 @@ exports.deleteComment = catchAsync(async (req, res, next) => {
   if (!comment) {
     return next(new AppError('no comment with that id', 404));
   }
-  if (comment.user.toString() != req.user.id.toString()) {
+  if (comment.user.id != req.user.id) {
     return next(new AppError('you are not allowed to delete this comment', 403));
   }
   await userModel.findByIdAndUpdate(req.user.id,
@@ -173,7 +173,7 @@ exports.saveComment = catchAsync(async (req, res, next) => {
     {$pull: {'savedPostsAndComments.comments': req.params.id}};
   await userModel.findByIdAndUpdate(req.user.id, update, {new: true});
   res.status(200).json({
-    status: 'success',    
+    status: 'success',
   });
 });
 
