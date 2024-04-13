0b1c6c55c5ef6eeacc1be09f74b85be19d3b41e2
Moaz Hashem <moaaz.m.hashem@gmail.com>
2024-04-13 19:25:19 +0200

    Update postcontroller.js

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -27,6 +27,7 @@ exports.getBestPosts = catchAsync(async (req, res, next) => {
     },
   });
 });
+
 exports.getSubredditPosts = catchAsync(async (req, res, next) => {
   const pageNumber = req.query.page || 1;
   const posts = paginate.paginate(await postModel.find({
@@ -39,6 +40,7 @@ exports.getSubredditPosts = catchAsync(async (req, res, next) => {
     },
   });
 });
+
 exports.sharePost= catchAsync(async (req, res, next) => {
   const destination = req.body.destination;
   if (!req.body.postid) {
@@ -107,6 +109,7 @@ exports.sharePost= catchAsync(async (req, res, next) => {
     status: 'success',
   });
 });
+
 exports.getPost = catchAsync(async (req, res, next) => {
   const post = await postModel.findById(req.params.postid);
   if (!post) {
@@ -154,6 +157,7 @@ exports.deletePost = catchAsync(async (req, res, next) => {
 });
 
 exports.vote = handlerFactory.voteOne(postModel, 'posts');
+
 exports.lockPost = catchAsync(async (req, res, next) => {
   const post = await postModel.findById(req.params.postid);
   if (!post) {
@@ -168,6 +172,7 @@ exports.lockPost = catchAsync(async (req, res, next) => {
     status: 'success',
   });
 });
+
 exports.savePost = catchAsync(async (req, res, next) => {
   const post= await postModel.findById(req.params.postid);
   if (!post) {
@@ -183,6 +188,7 @@ exports.savePost = catchAsync(async (req, res, next) => {
     status: 'success',
   });
 });
+
 exports.hidePost = catchAsync(async (req, res, next) => {
   const post = await postModel.findById(req.params.postid);
   if (!post) {
@@ -216,6 +222,7 @@ exports.unhidePost = catchAsync(async (req, res, next) => {
     },
   });
 });
+
 exports.markNSFW = catchAsync(async (req, res, next) => {
   const post = await postModel.findById(req.params.postid);
   if (!post) {
@@ -230,6 +237,7 @@ exports.markNSFW = catchAsync(async (req, res, next) => {
     status: 'success',
   });
 });
+
 exports.markSpoiler = catchAsync(async (req, res, next) => {
   const post = await postModel.findById(req.params.postid);
   if (!post) {
@@ -390,5 +398,6 @@ exports.createPost = catchAsync(async (req, res, next) => {
     },
   });
 });
+
 exports.reportPost = catchAsync(async (req, res, next) => {}); // TODO NEED MODERATION
 exports.crosspost = catchAsync(async (req, res, next) => {});
