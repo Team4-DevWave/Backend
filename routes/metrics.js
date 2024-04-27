ec7dee4200670dca0c83c9b8c07ab033771865d2
Hassanhatem1 <hassan.hatem1282@gmail.com>
2024-04-27 20:24:16 +0300

    hopefully last update

--- routes/metrics.js
+++ routes/metrics.js
@@ -30,7 +30,7 @@ router.use((req, res, next) => {
 
   res.on('finish', () => {
     const duration = Date.now() - start;
-    if (req.headers.host === 'www.threadit.tech' && req.path !== '/metrics') {
+    if (req.headers.host === 'www.threadit.tech' && req.path !== '/api/v1/metrics') {
       requestsCounter.inc();
       latencyHistogram.observe(duration);
     }
