edb8c09d6380d97979e007ecb7ecc3e1fcbca2de
mariamgamal70 <mariamgamal70@gmail.com>
2024-04-29 20:46:47 +0300

    socket server running supposedly

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 const {Server} = require('socket.io');
+const http = require('http');
 dotenv.config({path: './config.env'});
 
 const DB = process.env.DATABASE.replace(
@@ -19,15 +20,16 @@ mongoose
     });
 
 const port = process.env.PORT || 8000;
-const server= app.listen(port, () => {
+app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
-
-const io = new Server(server,
+const socketPort = 3005;
+const socketServer = http.createServer(); // Create a new HTTP server for the Socket.IO server
+const io = new Server(socketServer,
     {
-      pingTimeout: 60000,
+      // pingTimeout: 60000,
       cors: {
-        origin: 'http://localhost:3005',
+        origin: 'http://localhost:3000/',
       },
     });
 io.on('connection', (socket) => {
@@ -35,18 +37,27 @@ io.on('connection', (socket) => {
     socket.join(userData.id);
     socket.emit('connected');
   });
-  socket.on('join room', (room) => {
-    socket.join(room);
+  socket.on('error', (error) => {
+    console.log('Socket Error', error);
   });
-  socket.on('typing', (room) => socket.in(room).emit('typing'));
-  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+  socket.on('join room', (roomID) => {
+    socket.join(roomID);// recieve the room id
+  });
+  socket.on('typing', (roomID) => socket.in(roomID).emit('typing'));
+  socket.on('stop typing', (roomID) => socket.in(roomID).emit('stop typing'));
 
   socket.on('new message', (newMessageRecieve) => {
-    const chat = newMessageRecieve.chatID;
-    if (!chat.chatroomMembers) console.log('chats.chatroomMembers is not defined');
-    chat.chatroomMembers.forEach((user) => {
-      if (user._id == newMessageRecieve.sender._id) return;
-      socket.in(user._id).emit('message recieved', newMessageRecieve);
-    });
+    const chat = newMessageRecieve;
+    console.log('sender id11'+newMessageRecieve.chatID);
+    console.log('hello form the other side'+chat.chatroomMembers);
+    if (!chat.chatroomMembers) console.log('hello empty');
+    // chat.chatroomMembers.forEach((user) => {
+    // if (1) return; // hhhhhh
+    // console.log('sender id: '+newMessageRecieve.sender._id);
+    // console.log('message'+newMessageRecieve.message);
+    socket.in(newMessageRecieve.chatID).emit('message recieved', newMessageRecieve);
   });
+});
+socketServer.listen(socketPort, () => { // Start the Socket.IO server on port 3005
+  console.log(`Socket.IO server running on port ${socketPort}...`);
 });
