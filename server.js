b61f6c91eb06842454915fa6e20ee2e651619574
mariamgamal70 <mariamgamal70@gmail.com>
2024-04-29 22:36:03 +0300

    socket functioning

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const app = require('./app');
 const {Server} = require('socket.io');
 const http = require('http');
+const chatroomModel = require('./models/chatroommodel');
 dotenv.config({path: './config.env'});
 
 const DB = process.env.DATABASE.replace(
@@ -46,16 +47,16 @@ io.on('connection', (socket) => {
   socket.on('typing', (roomID) => socket.in(roomID).emit('typing'));
   socket.on('stop typing', (roomID) => socket.in(roomID).emit('stop typing'));
 
-  socket.on('new message', (newMessageRecieve) => {
+  socket.on('new message', async (newMessageRecieve) => {
     const chat = newMessageRecieve;
-    console.log('sender id11'+newMessageRecieve.chatID);
-    console.log('hello form the other side'+chat.chatroomMembers);
-    if (!chat.chatroomMembers) console.log('hello empty');
-    // chat.chatroomMembers.forEach((user) => {
-    // if (1) return; // hhhhhh
-    // console.log('sender id: '+newMessageRecieve.sender._id);
-    // console.log('message'+newMessageRecieve.message);
-    socket.in(newMessageRecieve.chatID).emit('message recieved', newMessageRecieve);
+    console.log(chat);
+    if (!chat.chatroomMembers) {
+      console.log('hello empty');
+      return;
+    }
+    socket.in(newMessageRecieve.chatID).emit('message recieved', (message)=>{
+      console.log('Received a message:', message);
+    });
   });
 });
 socketServer.listen(socketPort, () => { // Start the Socket.IO server on port 3005
