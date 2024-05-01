d507dac1e1415c5a1237c49ffa6d9025ce62604b
Hassanhatem1 <hassan.hatem1282@gmail.com>
2024-05-01 20:59:35 +0300

    merge with main + NEW METRIC

--- routes/metrics.js
+++ routes/metrics.js
@@ -1,38 +1,35 @@
 /// By DevOps For Monitoring The Application
 
-const express = require('express');
-const client = require('prom-client');
+const express = require("express");
+const client = require("prom-client");
 
 const router = express.Router();
 
 // Create a new counter metric for tracking the number of requests
 const requestsCounter = new client.Counter({
-  name: 'requests_total',
-  help: 'Total number of requests',
+  name: "requests_total",
+  help: "Total number of requests",
 });
 
-// Create a new histogram metric for tracking request latency
-const latencyHistogram = new client.Histogram({
-  name: 'request_latency',
-  help: 'Latency of requests',
-  buckets: [0.01, 0.1, 5, 15, 50, 100, 500],  
+// Create a new summary metric for tracking request latency
+const latencySummary = new client.Summary({
+  name: "request_latency",
+  help: "Latency of requests",
+  percentiles: [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99],
 });
-
-// Create a new counter metric for tracking error rates
-// const errorCounter = new client.Counter({
-//   name: 'errors_total',
-//   help: 'Total number of errors',
-// });
 
 // A middleware for counting requests and measuring latency
 router.use((req, res, next) => {
   const start = Date.now();
 
-  res.on('finish', () => {
+  res.on("finish", () => {
     const duration = Date.now() - start;
-    if (req.headers.host === 'www.threadit.tech' && req.path !== '/api/v1/metrics') {
+    if (
+      req.headers.host === "www.threadit.tech" &&
+      req.path !== "/api/v1/metrics"
+    ) {
       requestsCounter.inc();
-      latencyHistogram.observe(duration);
+      latencySummary.observe(duration);
     }
     console.log(`Request to ${req.headers.host}${req.path}`);
   });
@@ -40,19 +37,9 @@ router.use((req, res, next) => {
   next();
 });
 
-// // An example route that sometimes errors
-// router.get('/example', (req, res) => {
-//   if (Math.random() < 0.1) {  // 10% of requests will result in an error
-//     errorCounter.inc();
-//     res.status(500).send('An error occurred');
-//   } else {
-//     res.send('Success');
-//   }
-// });
-
 // Create a metrics endpoint
-router.get('/', async (req, res) => {
-  res.set('Content-Type', client.register.contentType);
+router.get("/", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
   const metrics = await client.register.metrics();
   res.end(metrics);
 });
