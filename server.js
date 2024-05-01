28cd803d76e44da5784a5cc0cb381a442ce775b5
MohamedAhmed18 <modyben21@yahoo.com>
2024-05-02 00:37:21 +0300

    did real time chat

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = require('./app');
 const {Server} = require('socket.io');
 const http = require('http');
 const chatroomModel = require('./models/chatroommodel');
+const chatMessageModel = require('./models/chatmessagemodel');
 const jwt = require('jsonwebtoken');
 dotenv.config({path: './config.env'});
 
@@ -16,6 +17,59 @@ mongoose
     .connect(DB)
     .then(() => {
       console.log('db connection success');
+      const socketPort = 3005;
+      const socketServer = http.createServer(); // Create a new HTTP server for the Socket.IO server
+      const io = new Server(socketServer,
+          {
+            pingTimeout: 600000,
+            cors: {
+              origin: 'http://localhost:3000', // removed a /   to test
+            },
+          });
+      io.use((socket, next) => {
+        const token = socket.handshake.query.token;
+        try {
+          const decoded = (jwt.verify)(token, process.env.JWT_SECRET);
+          socket.userID = decoded.userID;
+          next();
+        } catch (err) {
+          console.log(err);
+        }
+      });
+      io.on('connection', (socket) => {
+        socket.on('join rooms', async () => {
+          // Check if a chatroom with the given ID exists
+          const rooms = await chatroomModel.find({chatroomMembers: {$in: [socket.userID]}}).select('_id').exec();
+          const roomIds = rooms.map((room) => room._id.toString());
+          const joinedRooms = Array.from(socket.rooms);
+          joinedRooms.forEach((roomId) => {
+            if (!roomIds.includes(roomId)) {
+              socket.leave(roomId);
+            }
+          });
+          roomIds.forEach((roomId) => socket.join(roomId));
+        });
+        socket.on('leave room', (roomID) => {
+          socket.leave(roomID);
+        });
+        socket.on('new message', async (message) => {
+          const room=await chatroomModel.findOne({chatroomMembers: {$in: [socket.userID]},
+            _id: message.roomID});
+          if (room) {
+            io.in(message.roomID).emit('message received', message.content);
+            const chatMessage = await chatMessageModel.create({
+              sender: socket.userID,
+              message: message.content,
+              chatID: room._id,
+            });
+            room.latestMessage = chatMessage._id;
+            await room.save();
+          }
+        });
+      });
+      socketServer.listen(socketPort, () => { // Start the Socket.IO server on port 3005
+        console.log(`Socket.IO server running on port ${socketPort}...`);
+      });
     })
     .catch((err) => {
       console.log('ERROR');
@@ -24,46 +78,4 @@ mongoose
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
-});
-const socketPort = 3005;
-const socketServer = http.createServer(); // Create a new HTTP server for the Socket.IO server
-const io = new Server(socketServer,
-    {
-      pingTimeout: 600000,
-      cors: {
-        origin: 'http://localhost:3000', // removed a /   to test
-      },
-    });
-io.use((socket, next) => {
-  const token = socket.handshake.query.token;
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return next(new Error('Authentication error'));
-    }
-    socket.userID = decoded.id;
-    next();
-  });
-});
-io.on('connection', (socket) => {
-  socket.on('setup', () => {
-    socket.join(socket.userID);
-    socket.emit('connected', socket.userID);
-  });
-  socket.on('join room', async (roomID) => {
-    // Check if a chatroom with the given ID exists
-    const chatroom = await chatroomModel.findById(roomID);
-    if (!chatroom) {
-      return socket.emit('error', 'Chatroom not found');
-    }
-    socket.join(roomID);// validation needed
-  });
-  socket.on('leave room', (roomID) => {
-    socket.leave(roomID);
-  });
-  socket.on('new message', (message) => {
-    io.in(message.roomID).emit('message received', message.content);
-  });
-});
-socketServer.listen(socketPort, () => { // Start the Socket.IO server on port 3005
-  console.log(`Socket.IO server running on port ${socketPort}...`);
 });
