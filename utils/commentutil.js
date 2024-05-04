0212438d2cc68b360eb402e93def1adb6051dece
MohamedAhmed18 <modyben21@yahoo.com>
2024-05-04 17:51:07 +0300

    Revert "Update commentutil.js"
    This reverts commit 3e5d1ed50a81921ab37bd4b1655c9c2fe16a9290.

--- utils/commentutil.js
+++ utils/commentutil.js
@@ -20,9 +20,8 @@ exports.alterComments = async (req, comments) => {
   const newComments = [];
   console.log(user.username);
 
-  for (const commenty of comments) {
+  for (const comment of comments) {
     // const commentUser=comment.user.id;
-    const comment = commenty.toObject();
     const commentPost=await postModel.findById(comment.post);
     if (commentPost && commentPost.subredditID) { // check access to private subreddit
       const subredditID=commentPost.subredditID.id;
